63bf334744c1a74bf2d9717392bde27eed09bf16
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-03-27 02:59:37 +0100

    feat: implement activity refresh mode mod

--- app/src/main/java/me/timschneeberger/onyxtweaks/mods/global/PerActivityRefreshModes.kt
+++ app/src/main/java/me/timschneeberger/onyxtweaks/mods/global/PerActivityRefreshModes.kt
@@ -7,17 +7,22 @@ import com.github.kyuubiran.ezxhelper.finders.MethodFinder
 import com.github.kyuubiran.ezxhelper.misc.AndroidUtils
 import com.onyx.android.sdk.device.Device
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import me.timschneeberger.onyxtweaks.R
 import me.timschneeberger.onyxtweaks.mods.Constants.GLOBAL
 import me.timschneeberger.onyxtweaks.mods.base.ModPack
 import me.timschneeberger.onyxtweaks.mods.base.TargetPackages
 import me.timschneeberger.onyxtweaks.mods.utils.createAfterHookCatching
 import me.timschneeberger.onyxtweaks.mods.utils.firstByName
+import me.timschneeberger.onyxtweaks.ui.model.ActivityRule
 import me.timschneeberger.onyxtweaks.utils.PreferenceGroups
 
 @TargetPackages(GLOBAL)
 class PerActivityRefreshModes : ModPack() {
-    override val group = PreferenceGroups.NONE
+    override val group = PreferenceGroups.PER_ACTIVITY_SETTINGS
 
+    // Note: Do not modify the enum value names, as they are serialized to preferences
     enum class UpdateOption(val value: Int) {
         UNKNOWN(-1),
         DEFAULT(0),
@@ -27,26 +32,33 @@ class PerActivityRefreshModes : ModPack() {
         X(4)
     }
 
+    private fun findRules(pkgName: String) =
+        preferences.get<String>(R.string.key_per_activity_settings).run {
+            try {
+                Json.decodeFromString<List<ActivityRule>>(this)
+                    .filter { it.packageName == pkgName }
+            } catch (e: SerializationException) {
+                Log.ex("Failed to load activity rules for $pkgName: $e")
+                emptyList()
+            }
+        }
+
     override fun handleLoadPackage(lpParam: XC_LoadPackage.LoadPackageParam) {
         MethodFinder.fromClass(Activity::class.java)
             .firstByName("onResume")
             .createAfterHookCatching { param ->
-                Log.ex("Resumed: ${param.thisObject::class.java.name}")
-                if(!lpParam.packageName.contains("komikku"))
-                    return@createAfterHookCatching
-
-                // Delay to allow Onyx's onResume hook to switch the currently cached component name,
-                // otherwise the previous component will be modified
-                AndroidUtils.mainHandler.postDelayed({
-                    if (param.thisObject::class.java.simpleName == "ReaderActivity")
-                        EInkHelper.setAppScopeRefreshMode(UpdateOption.REGAL.value)
-                    else
-                        EInkHelper.setAppScopeRefreshMode(UpdateOption.DU.value)
-
-                    Log.ex("Refresh mode set to ${Device.currentDevice().appScopeRefreshMode}")
-                }, 500)
-
-                Log.ex("Refresh mode set to ${Device.currentDevice().appScopeRefreshMode}")
+                findRules(lpParam.packageName).let { rules ->
+                    var rule = rules.firstOrNull { it.activityClass.also { string -> Log.ex("Comparing $string to ${param.thisObject::class.java.name}") } == param.thisObject::class.java.name }
+                    rule = rule ?: rules.firstOrNull { it.activityClass == null }
+                    rule?.let { rule ->
+                        // Delay to allow Onyx's onResume hook to switch the currently cached component name,
+                        // otherwise the previous component will be modified
+                        AndroidUtils.mainHandler.postDelayed({
+                            EInkHelper.setAppScopeRefreshMode(rule.updateMode.value)
+                            Log.dx("Refresh mode set to ${Device.currentDevice().appScopeRefreshMode}")
+                        }, 500)
+                    }
+                }
             }
     }
 }
