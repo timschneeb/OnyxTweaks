c74e6be10ecb883d21c3c09a14fa8ca9f190ca8e
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-04-04 19:20:59 +0200

    feat: add shortcut to lsposed log

--- app/src/main/java/me/timschneeberger/onyxtweaks/ui/fragments/SettingsMiscFragment.kt
+++ app/src/main/java/me/timschneeberger/onyxtweaks/ui/fragments/SettingsMiscFragment.kt
@@ -1,9 +1,26 @@
 package me.timschneeberger.onyxtweaks.ui.fragments
 
+import androidx.preference.Preference
+import com.topjohnwu.superuser.Shell
+import me.timschneeberger.onyxtweaks.R
 import me.timschneeberger.onyxtweaks.ui.activities.SettingsActivity
 import me.timschneeberger.onyxtweaks.ui.preferences.PreferenceGroup
 import me.timschneeberger.onyxtweaks.utils.PreferenceGroups
 
 
 @PreferenceGroup(PreferenceGroups.MISC)
-class SettingsMiscFragment : SettingsBaseFragment<SettingsActivity>()
+class SettingsMiscFragment : SettingsBaseFragment<SettingsActivity>() {
+    private val shareLogs by lazy { findPreference<Preference>(getString(R.string.key_misc_share_debug_logs)) }
+
+    override fun onConfigurePreferences() {
+        shareLogs?.setOnPreferenceClickListener {
+            Shell.getShell { shell ->
+                shell.newJob().add(
+                    "am start-activity -a android.intent.action.MAIN -p com.android.shell -n com.android.shell/.BugreportWarningActivity " +
+                            "-c org.lsposed.manager.LAUNCH_MANAGER -d logs"
+                ).exec()
+            }
+            true
+        }
+    }
+}
