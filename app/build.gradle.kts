a35d203afd14f143a805b7027b6103d4c2c3144b
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-04-04 23:08:42 +0200

    chore: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 4
-        versionName = "1.3.0"
+        versionName = "1.2.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
