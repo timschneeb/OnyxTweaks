d2ec59912e93505195d27a1cb34d34118ce32752
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-04-16 17:19:48 +0200

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.timschneeberger.onyxtweaks"
         minSdk = 28
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.5.0"
+        versionCode = 8
+        versionName = "1.5.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
