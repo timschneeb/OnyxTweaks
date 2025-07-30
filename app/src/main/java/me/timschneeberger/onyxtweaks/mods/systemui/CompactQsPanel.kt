227b11988c368a6bb9f9fcc8cb098bfbcd0146b9
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-07-30 19:22:35 +0200

    fix(CompactQsPanel): 4.1 support

--- app/src/main/java/me/timschneeberger/onyxtweaks/mods/systemui/CompactQsPanel.kt
+++ app/src/main/java/me/timschneeberger/onyxtweaks/mods/systemui/CompactQsPanel.kt
@@ -1,12 +1,25 @@
 package me.timschneeberger.onyxtweaks.mods.systemui
 
+import android.view.View
+import androidx.core.view.updatePadding
+import com.github.kyuubiran.ezxhelper.EzXHelper.appContext
+import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import de.robv.android.xposed.callbacks.XC_InitPackageResources
+import de.robv.android.xposed.callbacks.XC_LoadPackage
 import me.timschneeberger.onyxtweaks.R
 import me.timschneeberger.onyxtweaks.mod_processor.TargetPackages
 import me.timschneeberger.onyxtweaks.mods.Constants.SYSTEM_UI_PACKAGE
 import me.timschneeberger.onyxtweaks.mods.base.IResourceHook
 import me.timschneeberger.onyxtweaks.mods.base.ModPack
+import me.timschneeberger.onyxtweaks.mods.utils.createAfterHookCatching
+import me.timschneeberger.onyxtweaks.mods.utils.dpToPx
+import me.timschneeberger.onyxtweaks.mods.utils.findClass
+import me.timschneeberger.onyxtweaks.mods.utils.firstByName
 import me.timschneeberger.onyxtweaks.utils.PreferenceGroups
+import me.timschneeberger.onyxtweaks.utils.Version.Companion.toVersion
+import me.timschneeberger.onyxtweaks.utils.castNonNull
+import me.timschneeberger.onyxtweaks.utils.onyxVersion
 
 /**
  * This mod pack enables a compact Quick Settings panel normally
@@ -16,6 +29,24 @@ import me.timschneeberger.onyxtweaks.utils.PreferenceGroups
 @TargetPackages(SYSTEM_UI_PACKAGE)
 class CompactQsPanel : ModPack(), IResourceHook {
     override val group = PreferenceGroups.QS
+
+    override fun handleLoadPackage(lpParam: XC_LoadPackage.LoadPackageParam) {
+        if (onyxVersion < "4.1".toVersion())
+            return
+
+        // On v4.1+, the QS menu is missing bottom padding when in compact mode.
+        // This adds the padding back to the bottom of the QS panel.
+        findClass("com.android.systemui.qs.QSPanel")
+            .methodFinder()
+            .firstByName("initialize")
+            .createAfterHookCatching<HideQsComponents> { param ->
+                param.thisObject.objectHelper()
+                    .getObjectOrNull("controlCenterView")
+                    .castNonNull<View>()
+                    .updatePadding(bottom = appContext.dpToPx(16))
+            }
+    }
+
 
     override fun handleInitPackageResources(param: XC_InitPackageResources.InitPackageResourcesParam) {
         val enable = preferences.get<Boolean>(R.string.key_qs_panel_compact)
